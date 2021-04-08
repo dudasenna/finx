17f5f412af9424028a32cfdb5573f9cefd850921
Palloma Ramos <pallomaramos@MacBook-Pro-de-Palloma.local>
2021-04-08 14:55:35 -0300

    ajeita autolayout

--- Finx - Agamenon/AppSurdo/Views/LevelsViewController.swift
+++ Finx - Agamenon/AppSurdo/Views/LevelsViewController.swift
@@ -56,7 +56,7 @@ class LevelsViewController: UIViewController {
         
         buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
         buttonHard.layer.cornerRadius = 10
-        buttonHard.setTitle("Dificil", for: .normal)
+        buttonHard.setTitle("Difícil", for: .normal)
         buttonHard.titleLabel?.font = UIFont(name: "Raleway-Bold" , size: 30)
         buttonHard.setTitleColor(.white, for: .normal)
         buttonHard.setTitleColor(.orange, for: .selected)
@@ -86,24 +86,23 @@ class LevelsViewController: UIViewController {
         buttonMedium.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.35).isActive = true
         buttonMedium.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.12).isActive = true
         
-       labelNivel.bottomAnchor.constraint(equalTo: buttonEasy.topAnchor, constant: -100).isActive = true
+        labelNivel.bottomAnchor.constraint(equalTo: buttonEasy.topAnchor, constant: -70).isActive = true
         labelNivel.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         labelNivel.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 1).isActive = true
         labelNivel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
         
         
-        buttonEasy.bottomAnchor.constraint(equalTo: buttonMedium.bottomAnchor, constant: -120).isActive = true
+        buttonEasy.bottomAnchor.constraint(equalTo: buttonMedium.bottomAnchor, constant: -150).isActive = true
         buttonEasy.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         buttonEasy.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.35).isActive = true
         buttonEasy.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.12).isActive = true
         
-        
-        buttonHard.topAnchor.constraint(equalTo: buttonMedium.topAnchor, constant: 120).isActive = true
+        buttonHard.topAnchor.constraint(equalTo: buttonMedium.topAnchor, constant: 150).isActive = true
         buttonHard.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         buttonHard.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.35).isActive = true
         buttonHard.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.12).isActive = true
         
-        okLevelButton.topAnchor.constraint(equalTo: buttonHard.bottomAnchor, constant: 80).isActive = true
+        okLevelButton.topAnchor.constraint(equalTo: buttonHard.bottomAnchor, constant: 70).isActive = true
         okLevelButton.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         okLevelButton.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3).isActive = true
         okLevelButton.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.05).isActive = true
@@ -114,20 +113,20 @@ class LevelsViewController: UIViewController {
         cubes_num = 3
         
         if buttonEasy.isSelected {
-                    // set deselected
+            // set deselected
             buttonEasy.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
             okLevelButton.isEnabled = false
             buttonEasy.isSelected = false
-                } else {
-                    // set selected
-                    okLevelButton.isEnabled = true
-                    buttonEasy.backgroundColor = .white
-                    buttonMedium.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonEasy.isSelected = true
-                    buttonMedium.isSelected = false
-                    buttonHard.isSelected = false
-                }
+        } else {
+            // set selected
+            okLevelButton.isEnabled = true
+            buttonEasy.backgroundColor = .white
+            buttonMedium.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonEasy.isSelected = true
+            buttonMedium.isSelected = false
+            buttonHard.isSelected = false
+        }
         
     }
     @objc func buttonMediumAction(sender: UIButton!) {
@@ -135,20 +134,20 @@ class LevelsViewController: UIViewController {
         cubes_num = 5
         
         if buttonMedium.isSelected {
-                    // set deselected
+            // set deselected
             buttonMedium.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
             okLevelButton.isEnabled = false
             buttonMedium.isSelected = false
-                } else {
-                    // set selected
-                    okLevelButton.isEnabled = true
-                    buttonMedium.backgroundColor = .white
-                    buttonEasy.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonEasy.isSelected = false
-                    buttonMedium.isSelected = true
-                    buttonHard.isSelected = false
-                }
+        } else {
+            // set selected
+            okLevelButton.isEnabled = true
+            buttonMedium.backgroundColor = .white
+            buttonEasy.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonEasy.isSelected = false
+            buttonMedium.isSelected = true
+            buttonHard.isSelected = false
+        }
         
     }
     @objc func buttonHardAction(sender: UIButton!) {
@@ -156,20 +155,20 @@ class LevelsViewController: UIViewController {
         cubes_num = 7
         
         if buttonHard.isSelected {
-                    // set deselected
+            // set deselected
             buttonHard.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
             okLevelButton.isEnabled = false
             buttonHard.isSelected = false
-                } else {
-                    // set selected
-                    okLevelButton.isEnabled = true
-                    buttonHard.backgroundColor = .white
-                    buttonEasy.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonMedium.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
-                    buttonEasy.isSelected = false
-                    buttonMedium.isSelected = false
-                    buttonHard.isSelected = true
-                }
+        } else {
+            // set selected
+            okLevelButton.isEnabled = true
+            buttonHard.backgroundColor = .white
+            buttonEasy.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonMedium.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+            buttonEasy.isSelected = false
+            buttonMedium.isSelected = false
+            buttonHard.isSelected = true
+        }
     }
     
     
